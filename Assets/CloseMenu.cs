d8ca84f09b7b6bcd6cb22855f619f9f8a1ac8e74
Paula <paulitacv25@gmail.com>
2019-07-01 15:41:27 -0500

    Editar detalle

---
+++ Assets/CloseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Clooseee");
+        Destroy(transform.parent.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
